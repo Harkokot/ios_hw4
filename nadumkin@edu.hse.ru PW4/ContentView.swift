387d923f8afca65c06346a3fd76468feda3ef93b
ndumkin <79084750+ndumkin@users.noreply.github.com>
2022-10-08 16:45:15 +0300

    Initial Commit

---
+++ nadumkin@edu.hse.ru PW4/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  nadumkin@edu.hse.ru PW4
+//
+//  Created by Никита Думкин on 08.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nadumkin@edu.hse.ru PW4/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  nadumkin@edu.hse.ru PW4
+//
+//  Created by Никита Думкин on 08.10.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
